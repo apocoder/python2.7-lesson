25178d112b9459383cebb2d845b37ecb226fef30
abdullah <apocoder@gmail.com>
2016-09-17 10:31:05 -0700

    more variables and prinring

--- ex03.py
+++ ex03.py
@@ -15,3 +15,19 @@ print "Toplam sürülemeyen arabalar ",surucusuz_arabalar
 print "Toplam yolcu kapasitesi ", toplam_yolcu_kapasitesi
 print "Şuanda toplam ",yolcular," yolcumuz var."
 print "Bizim her arabaya ortalama ",araba_basina_dusen_ortalama_yolcu ," yolcu yerleştirmemiz gerekiyor"
+
+#print içersinde string format kullanmak 
+ismim = 'python'
+yasim = 46
+agirligim = 74 #kg
+boyum = 180 #cm
+goz_rengim = 'Siyah'
+saclarim = 'Kahverengi'
+
+print "Merhaba benim adım %s." % ismim
+print "Ağırlığım %d" % agirligim
+print "Boyum %d" % boyum
+print "Saçlarım %s , gözlerim %s" % (goz_rengim,saclarim)
+
+print "Ağırlığımı %d , Boyumu %d , Yaşımı %d toplarsak,%d elde ederiz" % (
+    agirligim,boyum,yasim,agirligim+boyum+yasim)
