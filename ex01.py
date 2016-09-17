79d9425f1f858aa12a4c6770ac75883bcf701f7e
abdullah <apocoder@gmail.com>
2016-09-17 09:13:50 -0700

    initial commit

---
+++ ex01.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+print "Hello World"
+print "Merhaba Dünya"
+
+#yorum satırı
+print "Merhaba ben Python"
+# bu yorum satırı için ise peki neden en üstteki bölüm çalıştı?
+#Bu durum python ın kendi içersindeki "hack" i diyebiliriz :)
---
+++ ex02.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+print "Toplam bilgisayar sayısı",65 + 30 / 5
+
+print "Monitör",20 + 18 / 3
+print "Klavye" ,100 +40 * 3 % 4
+
+print "Kablo sayısı"
+
+print 8 + 6 + 1 - 5 + 7 % 2 / 4 + 6
+
+print "Bu işlem doğru mu ? 3 + 2 < 5 - 7"
+
+print "Yanlış'mı yoksa?"
+
+print "3 + 2 = ?", 3 + 2
+print "5 - 7 = ?", 5 - 7
+
+print "İşlem önceliği ? 5 ", 6 / 2 + (5 - 3) == 5
+
+print "Daha fazlası ?"
+
+print "Büyük mü?", 5 > -2
+print "Büyük yada eşit mi?", 5 >= -2
+print "Küçük yada eşit mi?", 5 <= -2
+
+print "Peki 11 / 2 ?", 11 / 2
+
+print "Yoksa böyle mi  11 / 2 ?", 11 / 2.0
